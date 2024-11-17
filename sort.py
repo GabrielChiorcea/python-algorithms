e014a2d97d69d7c4297fb6e187c2150f724add3e
GabrielChiorcea() <107279421+GabrielChiorcea@users.noreply.github.com>
2024-11-17 22:06:30 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -28,6 +28,13 @@ def bsy(array, n):
 
 bsy(array, x)
 
+
+
+#Insert sort funcționează prin parcurgerea unei liste și inserarea fiecărui element în poziția corectă din secțiunea deja sortată a listei.
+#Algoritmul începe cu al doilea element al listei și îl compară cu elementele precedente, mutându-l spre stânga până când găsește locul potrivit.
+#Procesul se repetă pentru fiecare element din lista nesortată, până când întreaga listă devine sortată. Deși este simplu de înțeles și implementat, 
+#insert sort este eficient doar pentru liste mici sau aproape sortate, având o complexitate de timp de O(n2) în cel mai rău caz.
+
 def insert_sort(array):
      for i in range(1, len(array)):
          left_val = i -1
