e100a56586f024304011c77b0f8e500688e3a251
GabrielChiorcea() <107279421+GabrielChiorcea@users.noreply.github.com>
2024-11-17 22:31:02 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -96,3 +96,28 @@ def merge(left, right):
 unsortedArr = [3, 7, 6, -10, 15, 23.5, 55, -13]
 sortedArr = mergeSort(unsortedArr)
 print("Sorted array:", sortedArr)
+
+
+
+#Bubble sort este un algoritm de sortare simplu, dar ineficient pentru liste mari. Funcționează prin parcurgerea 
+#repetată a listei și compararea fiecărui pereche de elemente adiacente. Dacă două elemente sunt în ordine 
+#greșită (de exemplu, primul este mai mare decât al doilea), acestea sunt interschimbate. Acest proces continuă 
+#până când lista este parcurgă fără a se face nicio schimbare, semn că lista este sortată. Denumirea „bubble” provine din 
+#faptul că valorile mari „bulează” spre sfârșitul listei după fiecare iterație. Algoritmul are o complexitate de timp de 
+#O(n2) în cel mai rău și în cel mai bun caz, fiind astfel ineficient pentru liste mari.
+
+def bubble_sort(array):
+    for n in range(len(array) - 1, 0, -1):
+
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+                
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+print("Unsorted list is:")
+print(array)
+
+bubble_sort(array)
+
+print("Sorted list is:")
+print(array)
